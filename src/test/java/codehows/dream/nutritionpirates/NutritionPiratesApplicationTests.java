f63c9ca8794860b7ee72aada2d90b367f8819592
Nodpet <gupo941020@naver.com>
2024-06-15 13:24:24 +0900

    [FIX] contextLoad에 orderTest 추가

--- src/test/java/codehows/dream/nutritionpirates/NutritionPiratesApplicationTests.java
+++ src/test/java/codehows/dream/nutritionpirates/NutritionPiratesApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(classes = OrderControllerTest.class)
 class NutritionPiratesApplicationTests {
 
 	@Test
