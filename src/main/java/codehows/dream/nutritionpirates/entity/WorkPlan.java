97c51c025e11fda08559f6b94b2f313b0956a9f4
her0 <gupo941020@naver.com>
2024-06-16 11:22:11 +0900

    [ADD] workPlanA1

--- src/main/java/codehows/dream/nutritionpirates/entity/WorkPlan.java
+++ src/main/java/codehows/dream/nutritionpirates/entity/WorkPlan.java
@@ -1,21 +1,11 @@
 package codehows.dream.nutritionpirates.entity;
 
-import java.sql.Date;
-
 import codehows.dream.nutritionpirates.constants.Facility;
 import codehows.dream.nutritionpirates.constants.Process;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.sql.Date;
 
 @Entity
 @Getter
@@ -42,18 +32,15 @@ public class WorkPlan {
 	@JoinColumn(name = "lotCode")
 	private LotCode lotCode;
 
-//	@Column(nullable = false)
 	private String worker;
 
 	private String processCompletionTime;
 
-//	@Column(nullable = false)
 	private Date startTime;
 
 	private Date endTime;
 
 	@Enumerated(EnumType.STRING)
-//	@Column(nullable = false)
 	private Facility facility;
 
 	//반제품
---
+++ src/main/java/codehows/dream/nutritionpirates/workplan/process/A1WorkPlan.java
@@ -0,0 +1,19 @@
+package codehows.dream.nutritionpirates.workplan.process;
+
+import codehows.dream.nutritionpirates.constants.Facility;
+import codehows.dream.nutritionpirates.entity.WorkPlan;
+import codehows.dream.nutritionpirates.workplan.WorkPlans;
+
+public class A1WorkPlan implements WorkPlans {
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public WorkPlan createWorkPlan() {
+        return WorkPlan.builder()
+                .facility(Facility.washer)
+                .build();
+    }
+}
