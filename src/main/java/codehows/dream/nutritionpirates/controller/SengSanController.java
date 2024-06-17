37476837f0cbc4164aced24da7b2bd63e9389619
angrybird24 <angrybird24@naver.com>
2024-06-17 12:26:29 +0900

    생산관리 페이지 완료
    생산관리 페이지 만 완료

---
+++ src/main/java/codehows/dream/nutritionpirates/controller/SengSanController.java
@@ -0,0 +1,18 @@
+package codehows.dream.nutritionpirates.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class SengSanController {
+
+    @GetMapping(value = {"/sengsan","/sengsanCheck","/sengsancheck"})
+    public String getSengsanCheck(){
+        return "SengSan_check";
+    }
+    @GetMapping(value = {"plan","/sengsanPlan","/sengsanplan"})
+    public String getSengsanPlan(){
+        return "SengSan_Plan";
+    }
+
+
+}
---
+++ src/main/java/codehows/dream/nutritionpirates/controller/SuJuController.java
@@ -0,0 +1,20 @@
+package codehows.dream.nutritionpirates.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class SuJuController {
+
+    @GetMapping(value = {"/suju", "/suzu"})
+    public String getSuju() {
+        return "suzu";
+    }
+    @GetMapping(value = {"/suzucheck", "/sujucheck"})
+    public String getSujuCheck (){
+        return "suzu_check";
+    }
+    @GetMapping(value = {"/suzucheckOrderer", "/sujucheckOrderer"})
+    public String getSujuCheckOrderer (){
+        return "suzu_check_orderer";
+    }
+}
