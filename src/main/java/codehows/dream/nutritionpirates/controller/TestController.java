36012db182e94e56041d148f1f713a24c9dad8f5
jwshin <154314097+jwsh1n@users.noreply.github.com>
2024-06-21 00:42:20 +0900

    페이지네이션이 괴롭힘 ㅜ

--- src/main/java/codehows/dream/nutritionpirates/controller/TestController.java
+++ src/main/java/codehows/dream/nutritionpirates/controller/TestController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
     @GetMapping(value = "/")
     public String index() {
-        return "index";
+        return "test";
     }
 }
