38da8d6ecc2fb775b212d10161d745f0e11ca809
jwshin <154314097+jwsh1n@users.noreply.github.com>
2024-06-24 16:22:00 +0900

    중간1

--- src/main/java/codehows/dream/nutritionpirates/controller/TestController.java
+++ src/main/java/codehows/dream/nutritionpirates/controller/TestController.java
@@ -1,7 +1,9 @@
 package codehows.dream.nutritionpirates.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class TestController {
     @GetMapping(value = "/")
     public String index() {
